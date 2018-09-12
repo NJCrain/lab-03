a386cedc7bbc0a32d79af57ca4f9840b33deddf5
Nicholas Crain <nicholascrain@gmail.com>
2018-09-12 15:39:42 -0700

    added a 6th question that gives the user 4 attempts to guess the answer

--- js/app.js
+++ js/app.js
@@ -3,19 +3,6 @@
 var currentScore = 0;
 var scorePossible = 0;
 
-function correctAnswer(questionNumber) {
-  currentScore++;
-  scorePossible++;
-  alert('Correct! You have guessed ' + currentScore + ' out of ' + scorePossible + ' answers correctly');
-  console.log('Question ' + questionNumber + ': Correct');
-}
-
-function incorrectAnswer(questionNumber) {
-  scorePossible++;
-  alert('Incorrect! You have guessed ' + currentScore + ' out of ' + scorePossible + ' answers correctly');
-  console.log('Question ' + questionNumber + ': Incorrect');
-}
-
 var questionOne = prompt('Am I over 6\'2"?');
 if(questionOne.toUpperCase() === 'YES' || questionOne.toUpperCase() === 'Y'){
   currentScore++;
@@ -80,3 +67,29 @@ else {
   alert('Incorrect! You have guessed ' + currentScore + ' out of ' + scorePossible + ' answers correctly');
   console.log('Question 5: Incorrect');
 }
+
+/* increment scorePossible before the question to avoid multiple increments within the loop. The loop will ask how many step-siblings
+I have, giving the user 4 attempts to guess the right number */
+scorePossible++;
+for (var i = 0; i < 4; i++) {
+  var questionSix = prompt('How many step-siblings do I have? You have used ' + i + ' out of 4 guesses');
+  if(questionSix < 8) {
+    alert('Too Low, try again!');
+  }
+  else if (questionSix > 8) {
+    alert('Too high, try again!');
+  }
+  else if (questionSix === '8') {
+    currentScore++;
+    alert('Correct, I have 8 step siblings!');
+    console.log('Question 6: correct in '+ i + ' tries');
+    break;
+  }
+  else {
+    alert('Sorry, I couldn\'t understand your answer. Please try again');
+  }
+}
+alert('You have guessed ' + currentScore + ' out of ' + scorePossible + ' answers correctly');
+console.log('Question 6: Incorrect');
+
+
